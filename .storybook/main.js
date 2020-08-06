d4c07389131560f1d23fdaad1a0d68621c3182da
Uri Shaked <uri@urishaked.com>
2020-08-06 19:56:07 +0300

    chore(deps): storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-docs',
-    '@storybook/addon-google-analytics/register',
-    '@storybook/addon-knobs/register',
-    'storybook-events-logger/dist/register',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-knobs'],
 };
